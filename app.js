cd15e8858b3e23a27873c18c8439e19822e2d222
Bakytbek <93685132+kvadderr@users.noreply.github.com>
2022-01-25 16:57:19 +0600

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const orderRouter = require("./api/order/order.router");
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
